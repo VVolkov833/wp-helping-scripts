5e7c32e472b97ca578422a6cf62bc7f3d9e880d5
VVolkov833 <finnish.ru@gmail.com>
2020-09-14 01:14:35 +0300

    improved readme, progress on replace.php

--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
+# A number of scripts,
+## which help working with WordPress
 
-nowebp.php removes all *.webp files from specified and inheritor directories
+*Instructions are inside the files*
 
-nothumbs.php removes all wordpress thumbnail images from specified and inheritor directories
+###Bulk actions
 
-replace.php replaces all phrases, specified with a regular expression, from WP database (content) and theme files
+*nowebp.php* removes all *.webp files from specified and inheritor directories
+
+*nothumbs.php* removes all wordpress thumbnail images from specified and inheritor directories
+
+*replace.php* replaces all phrases, specified with a regular expression, from WP database (content) and theme files
--- replace.php
+++ replace.php
@@ -1,6 +1,9 @@
 <pre><?php
 
 /*
+++ add table prefix protection
+++ simple navigation
+++ on update error
 NOT READY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 1. specify the regular expression
 2. place the file to your wordpress root directory (next to wp-config.php file)
@@ -9,123 +12,145 @@ NOT READY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 */
 
-
-include_once( 'wp-config.php' );
-
-$mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-if (!$mysqli) {
-    exit;
-}
-$mysqli->set_charset( 'utf8' );
-
-//-------____________--___---____________________--------
-
-/*
-24-Stunden-Pflege
-24 Stunden Pflege
-24H Stunden Pflege
-24H-Pflege
-24-Stunden-Betreuung
-24 Stunden Betreuung
-24H Betreuung
-24H-Betreuung
-and same within quotes ”„
-//*/
+/* the example:
+input:
+    24-Stunden-Pflege
+    24 Stunden Pflege
+    24H Stunden Pflege
+    24H-Pflege
+    24-Stunden-Betreuung
+    24 Stunden Betreuung
+    24H Betreuung
+    24H-Betreuung
+    ”24-Stunden-Betreuung„
+    ”24-Stunden„ Pflege
+    a few more similar variants
+output:
+    sogenannte 24-Stunden-Pflege
+    sogenannte 24 Stunden Pflege
+    ...
+*/
 
 $pflege = new PregMatchReplace( // when making regexp, make sure it doesn't break everything on second attempt
     '/(?:sogenannte )*([”„]*24[- H]+(?:Stunden[”]?[- ]+)?(?:Pflege|Betreuung)[”]*)/i',
     'sogenannte $1'
 );
 
+$content = new TargetDatabase( 'posts', 'post_content', 'ID', $pflege, false,
+    '`post_status` = "publish"',
+    '`post_type` ASC, `id` ASC',
+    '100000'
+);
+
+$content->test();
+//$content->run();
 
 
-$posts = 'SELECT
+class TargetDatabase {
 
-`ID`,
-`post_content`,
-`post_title`,
-`post_type`
+    private $db, $t, $reg, $hideEmpty;
 
-FROM `'.$table_prefix.'posts`
-WHERE `post_status` = "publish"
-ORDER BY `post_type` ASC, `id` ASC
-LIMIT 100000';
+    public function __construct( $table, $contCol, $IDCol, $PMR, $hideEmpty = false, $where = '', $order = '', $limit = '' ) {
+    
+        include( 'wp-config.php' );
+        $this->db = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME );
+        if (!$this->db) {
+            exit;
+        }
+        $this->db->set_charset( 'utf8' );
 
-$posts = queryToArray( $posts );
-
-$total = 0;
-
-foreach ( $posts as $v ) {
-
-    if ( isset( $_GET['replace'] ) ) {
-/*
-        $result = $mysqli->query( '
-            UPDATE `'.$table_prefix.'posts`
-            SET `post_content` = "'.$mysqli->real_escape_string( $content ).'"
-            WHERE `ID` = "'.$v['ID'].'"
-        ');
+        $this->t = (object) [
+            'table'   => $table,
+            'contCol' => $contCol,
+            'IDCol'   => $IDCol,
+            'prefix'  => $table_prefix,
+            'where'   => $where,
+            'order'   => $order,
+            'limit'   => $limit
+        ];
         
-        if ( $result === true ) {
-            echo ' <font color="#00ff00">success</font>'."\n";
-        } else {
-            echo ' <font color="#ff0000">update failed</font> '.$mysqli->error."\n";
-        }
-//*/
-    } else {
+        $this->reg = $PMR;
+        $this->hideEmpty = $hideEmpty;
 
-/*
-        $inTitle = $pflege->match( $v['post_title'] );
-        if ( $inTitle ) { // to hide not interesting
-            echo formatMatchOutput( array_merge( $v, [
-                'text_type' => 'title',
-                'found' => $inTitle
-            ] ) );
-            $total += $inTitle[1];
-        }
-//*/
-
-        $inContent = $pflege->match( $v['post_content'] );
-        if ( $inContent ) { // to hide not interesting
-            echo formatMatchOutput( array_merge( $v, [
-                'text_type' => 'content',
-                'found' => $inContent
-            ] ) );
-            $total += $inContent[1];
-        }
-            
-    }
-
-}
-
-echo "\n".'Total: '.$total;
-if ( isset( $_GET['replace'] ) ) {
-    echo "\n".'<a href="'.basename( $_SERVER['PHP_SELF'] ).'">&lt;&lt; Check again</a>';
-} else {
-    echo "\n".'<a href="?replace">Replace all &gt;&gt;</a>';
-}
-
-
-//____________---__----_____________________-------
-
-function queryToArray( $sql, $id = '' ) {
-    global $mysqli;
-
-    $result = $mysqli->query( $sql );
-
-    if ( $result->num_rows > 0 ) {
-        
-        while( $row = $result->fetch_assoc() ) {
-            if ( $id )
-                $return[$row[$id]] = $row;
-            else
-                $return[] = $row;
-        }
-
-        return $return;
     }
     
-    return false;
+    public function test() {
+        
+        $data = $this->select();
+        
+        foreach( $data as $v ) {
+
+            $match = $this->reg->match( $v[ $this->t->contCol ] );
+            if ( !$this->hideEmpty || $match ) {
+                echo $this->report( $v[ $this->t->IDCol ], $match['count'], $match['matches'] );
+            }
+        }
+
+    }
+    
+    public function run() {
+    
+    }
+    
+    private function select() {
+    
+        $posts = 'SELECT
+
+        `'.$this->t->IDCol.'`,
+        `'.$this->t->contCol.'`,
+
+        FROM `'.$this->t->prefix.$this->t->table.'`
+        '.( $where ? 'WHERE '.$this->t->where : '' ).'
+        '.( $order ? 'ORDER BY '.$this->t->order : '' ).'
+        '.( $limit ? 'LIMIT '.$this->t->limit : '' )
+
+        return $this->queryToArray( $posts );
+    }
+    
+    private function update( $id, $content ) {
+
+        if ( !$id )
+            return false;
+
+        $result = $this->db->query( '
+            UPDATE `'.$this->t->prefix.$this->t->table.'`
+            SET `'.$this->t->contCol.'` = "'.$this->db->real_escape_string( $content ).'"
+            WHERE `'.$this->t->IDCol.'` = "'.$id.'"
+        ');
+        
+        return $result;
+    }
+    
+    private function report( $id, $count = 0, $matches = [] ) {
+
+        return  'ID: '.$id."\t\t".''.( $count ? '' : ' NOT' ).'found or effected: '.$count.
+                '<font color="#'.( $count ? '00ff00' : 'ff0000' ).'">'.$count.'</font> '.
+                ( $this->t->table == 'posts' ? '<a href="\?p='.$id.'">&gt;&gt;</a>' : '' ).
+                ( $matches[0] ? "\n".implode( "\n", $matches ) : '' ).
+                "\n".'
+';
+    
+    }
+
+    private function queryToArray( $sql, $id = '' ) {
+
+        $result = $this->db->query( $sql );
+        if ( $result->num_rows > 0 ) {
+            while( $row = $result->fetch_assoc() ) {
+                if ( $id )
+                    $return[ $row[$id] ] = $row;
+                else
+                    $return[] = $row;
+            }
+            return $return;
+        }
+        return false;
+    }
+    
 }
+
+
+
 
 class PregMatchReplace {
 
@@ -139,25 +164,27 @@ class PregMatchReplace {
     public function match( $text ) {
         if ( preg_match_all( $this->from, $text, $matches, PREG_PATTERN_ORDER ) ) {
             if ( is_array( $matches[0] ) )
-                return [ $matches[0], count( $matches[0] ) ];
+                return [
+                    'matches' => $matches[0],
+                    'count'   => count( $matches[0] )
+                ];
             else
-                return false;
+                return [ 'matches' => false, 'count' => 0 ];
         }
     }
     
     public function replace( $text ) {
-        return [ preg_replace( $this->from, $this->to, $text, -1, $count ), $count ];
+        $result = preg_replace( $this->from, $this->to, $text, -1, $count );
+        if ( $count ) {
+            return [
+                'matches' => [],
+                'count' => $count
+            ];
+        } else {
+            return [ 'matches' => false, 'count' => 0 ];
+        }
     }
 
-}
-
-function formatMatchOutput($v) {
-    return  $v['post_type'].' ID '.$v['ID'].' '.
-            '<font color="#'.( $v['found'] ? '00ff00' : 'ff0000' ).'"> '.( $v['found'] ? $v['found'][1].' found' : 'not found' ).' in '.$v['text_type'].'</font> '.
-            '<font color="#888888">'.$v['post_title'].'</font> '.
-            '<a href="\?p='.$v['ID'].'">&gt;&gt;</a>'.
-            ( $v['found'] ? "\n".implode( "\n", $v['found'][0] ) : '' ).'
-';
 }
 
 ?>
